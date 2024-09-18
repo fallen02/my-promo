5345e872d6595124b6eccea0f7f9f007d57e93db
fallen02 <000.sb13@gmail.com>
2024-09-18 20:36:12 +0530

    :>

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+
+
+import icon from "astro-icon";
+
+
+
+import vue from "@astrojs/vue";
+
+
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({ applyBaseStyles: false }), react(), icon(), vue()],
+});

c44db8064b5b59974ffeddb316e1b2fab78f69c5
fallen02 <000.sb13@gmail.com>
2024-09-18 21:02:59 +0530

    :>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
-
-
 import icon from "astro-icon";
-
-
-
 import vue from "@astrojs/vue";
 
-
-
-import vercel from "@astrojs/vercel/serverless";
 
 
 

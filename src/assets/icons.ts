2430c6184ffbcdd4f311e096947e2c975d844319
fallen02 <000.sb13@gmail.com>
2024-09-21 02:54:14 +0530

    updated icons

---
+++ src/assets/icons.ts
@@ -0,0 +1,30 @@
+import idea from './icons/idea.svg'
+import technology from './icons/technology.png'
+import webdev from './icons/webdev.png'
+import seo from './icons/seo.png'
+import socialMediaMarkting from './icons/social-media-marketing.png'
+import graphicsDesign from './icons/graphics-design.png'
+import leadGenaration from './icons/lead-generation.png'
+import marketingAutomation from './icons/marketing-automation.png'
+import AIOptimization from './icons/ai-optimization.png'
+import contentWriting from './icons/content-writing.png'
+import contentMarketing from './icons/content-marketing.png'
+import collabMembership from './icons/membership.png'
+import salesforce from './icons/salesforce.png'
+
+
+export default { 
+    idea, 
+    technology, 
+    webdev, 
+    seo, 
+    socialMediaMarkting,
+    graphicsDesign,
+    leadGenaration,
+    marketingAutomation,
+    AIOptimization,
+    contentWriting,
+    contentMarketing,
+    collabMembership,
+    salesforce
+}
